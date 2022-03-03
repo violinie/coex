400ade59ebd206a041ef049daf5ef4185377825c
Hana Čopáková <sandracopakova@gmail.com>
2022-03-03 11:39:05 +0100

    create form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Form from "./Form";
 
 export default function Profile() {
   return (
-    <>
-      <div className="profile__header">
-        <h1 className="text-center profile__title m-0">Profil uchazeče</h1>
+    <div>
+      <div className="profile__header d-flex align-items-center">
+        <div>
+          <h1 className="text-center profile__title m-0">Profil uchazeče</h1>
+        </div>
       </div>
-      <form></form>
-    </>
+      <Form />
+    </div>
   );
 }
