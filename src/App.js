50cec1f1691674b698215d15f9b8180e4cd3da13
Hana Čopáková <sandracopakova@gmail.com>
2022-03-03 11:41:18 +0100

    create fake footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile";
 
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <Nav />
       <Profile />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer></footer>;
+  return <footer className="footer"></footer>;
 }
